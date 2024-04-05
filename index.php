efdc2be6d18cfdc6f28d9f40d0b5cc9fa453d201
Deeplovestha <diploveshrestha12345@gmail.com>
2024-04-05 13:00:06 +0545

    change name space

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
-      <h1>Deep Love Shrestha</h1>
+      <h1>DeepLove Shrestha</h1>
       <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
     </div>
   </section><!-- End Hero -->
